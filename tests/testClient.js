7e907e4f16524073a66a34c490e548df29dda678
Jayce Pulsipher <jaycemp@gmail.com>
2013-05-10 13:58:29 -0600

    Big ol changes. Simplified Server and moved input methods into separate module

--- tests/testClient.js
+++ tests/testClient.js
@@ -1,47 +1,45 @@
 'use strict';
 
 var logger = require('../index')
-	, fs = require('fs')
-	, testFile = fs.createWriteStream('./fallback.log', {flags: 'a'});
+    , fs = require('fs')
+    , testFile = fs.createWriteStream('./fallback.log', {flags: 'a'});
 
 var client1 = logger.createClient({
-		host: '192.168.1.120'
-	, port: 41234
-	, handler: 'test'
-	, fallback: testFile
+    host: '127.0.0.1'
+  , port: 5555
+  , handler: 'test'
 }, run1);
-
+/*
 var client2 = logger.createClient({
-		host: '192.168.1.120'
-	, port: 41234
-	, handler: 'test2'
+    host: '192.168.1.120'
+  , port: 41234
+  , handler: 'test2'
 }, run2);
 
 var client3 = logger.createClient({
-		host: '192.168.1.120'
-	, port: 41234
-	, handler: 'test3'
+        host: '192.168.1.120'
+    , port: 41234
+    , handler: 'test3'
 }, run3);
-
+*/
 var x = 0;
 
 function run1() {
-
-	client1.error('Test ' + x++);
-	setTimeout(run1, 500);
-
+    client1.log('Test ' + x++, x % 8);
+    setTimeout(run1, 500);
 }
-
+/*
 function run2() {
-	
-	client2.info('Derpy');
-	setTimeout(run2, 1000);
-	
+
+    client2.info('Derpy');
+    setTimeout(run2, 1000);
+
 }
 
 function run3() {
-	
-	client3.warn('ello mate');
-	setTimeout(run3, 2000);
-	
+
+    client3.warn('ello mate');
+    setTimeout(run3, 2000);
+
 }
+*/
--- tests/testServer.js
+++ tests/testServer.js
@@ -1,49 +1,44 @@
 'use strict';
 
 var logger = require('./../index')
-//	, lsAccess = new logger.Handler('test')
-	, extractorReg
-	, extractorMap;
+//    , lsAccess = new logger.Handler('test')
+    , extractorReg
+    , extractorMap;
 
 extractorReg = /^(([0-9]{1,3}\.){3}[0-9]{1,3}) ([0-9]{3}) ([0-9]+) ([a-z]+) "([^"]*)" ([0-9]\.[0-9]) "([^"]*)" "([^"]*)"$/i;
 extractorMap = [
-		'remote'
-	,	// Do not include second ip address capture
-	, 'status'
-	, 'duration'
-	, 'method'
-	, 'url'
-	, 'httpversion'
-	, 'referrer'
-	, 'useragent'
+        'remote'
+    ,    // Do not include second ip address capture
+    , 'status'
+    , 'duration'
+    , 'method'
+    , 'url'
+    , 'httpversion'
+    , 'referrer'
+    , 'useragent'
 ];
 
-var logServer = logger.createServer({
-		port: 41234
-}, function () {
-	
-	console.log('TCP listening on ' + this.tcp.address().port);
-	console.log('UDP listening on ' + this.udp.address().port);
-	
-});
+var logServer = logger.createServer();
+
+var logIn = logger.logger(logServer, { port: 5555 });
 
 var handler1 = logServer.createHandler('test');
 
-handler1.use(function (data) {
-	console.log('test: ' + data.message);
-});
+handler1.use(logger.console());
 
+/*
 var handler2 = logServer.createHandler('test2');
 
 handler2.use(function (data) {
-	console.log('test2: ' + data.message);
+    console.log('test2: ' + data.message);
 });
 
 var handler3 = logServer.createHandler('test3');
 
 handler3.use(function (data) {
-	console.log('test3: ' + data.message);
+    console.log('test3: ' + data.message);
 });
+*/
 
 //lsAccess.use(logger.extractor(extractorReg, extractorMap));
 
@@ -51,9 +46,9 @@ handler3.use(function (data) {
 var x = 0;
 
 lsAccess.use(function (msg) {
-	if (msg.dropped) {
-		console.log('Dropped');
-	}
+    if (msg.dropped) {
+        console.log('Dropped');
+    }
 });
 
 logger.setHandler(lsAccess);
