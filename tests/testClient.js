4bd53df3b8efe5d2674b53d6a89bf5822a01f223
Jayce Pulsipher <jaycemp@gmail.com>
2013-05-11 08:58:26 -0600

    Clean up client code
    Allow different tcp and udp ports
    Prototyped tcp.Socket.send
    Moved most functions out of Client initialize function

--- tests/testClient.js
+++ tests/testClient.js
@@ -25,7 +25,7 @@ var client3 = logger.createClient({
 var x = 0;
 
 function run1() {
-    client1.log('Test ' + x++, x % 8);
+    client1.log('Test ' + x++, x % );
     setTimeout(run1, 500);
 }
 /*
